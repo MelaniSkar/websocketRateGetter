7edce318c90e9f6d043c500fac092e088d33a05c
MelaniSkar <mellaniskar@gmail.com>
2018-09-20 18:05:18 +0300

    added colors

--- websocketRateGetter/src/main/webroot/js/app.js
+++ websocketRateGetter/src/main/webroot/js/app.js
@@ -10,14 +10,34 @@ function registerHandler() {
   eventBus = new EventBus('http://localhost:8080/eventbus');
   eventBus.onopen = function () {
     eventBus.registerHandler('publish_bitfinex_rate', function (error, message) {
-      const bitfinexRate = message.body;
-      document.getElementById('bitfinexRate').innerHTML = bitfinexRate;
+        const newRate = message.body;
+        const currentRate = Number(document.getElementById('bitfinexRate').innerHTML);
+        const del = newRate - currentRate;
+        if(del == 0) {
+            document.getElementById("bitfinexDel").style.color = "black";
+        } else if (del < 0) {
+            document.getElementById("bitfinexDel").style.color = "red";
+        } else
+            document.getElementById("bitfinexDel").style.color = "green";
+
+        document.getElementById('bitfinexRate').innerHTML = newRate;
+        document.getElementById('bitfinexDel').innerHTML = del;
     });
 
     eventBus.registerHandler('publish_bittrex_rate', function (error, message) {
-          const bittrexRate = message.body;
-          document.getElementById('bittrexRate').innerHTML = bittrexRate;
-        });
+        const newRate = message.body;
+        const currentRate = Number(document.getElementById('bittrexRate').innerHTML);
+        const del = newRate - currentRate;
+        if(del == 0) {
+            document.getElementById("bittrexDel").style.color = "black";
+        } else if (del < 0) {
+            document.getElementById("bittrexDel").style.color = "red";
+        } else
+            document.getElementById("bittrexDel").style.color = "green";
+
+        document.getElementById('bittrexRate').innerHTML = newRate;
+        document.getElementById('bittrexDel').innerHTML = del;
+    });
 
   }
 }
